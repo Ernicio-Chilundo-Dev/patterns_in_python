afbd41c0c2b0b010700d373f2a296ed9fa2f34d2
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-03-10 03:56:13 +0200

    added

--- 02.py
+++ 02.py
@@ -1,6 +1,13 @@
-rows = 25
+import os
+largura_tela = os.get_terminal_size().columns
+rows = 30
 for i in range(1,rows +1):
-    print(" " * (rows -i) + "*" * (2*i -1))
+    linha = " " * (rows -1) + "*" * (2*i -1)
+    espacos_extras = (largura_tela - len(linha))//2
+    print(" " * espacos_extras + linha)
     
-for i in range(rows -1,0,-1):
-    print(" " * (rows -i) + "*" * (2*i-1))
+
+for i in range(rows-1,0,-1):
+    linha = " " * (rows -1) + "*" * (2*i -1)
+    espacos_extras = (largura_tela - len(linha))//2
+    print(" " * espacos_extras + linha)
